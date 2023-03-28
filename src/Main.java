3e1379150e6020190e0f19d6b682959d60ff66e0
Yank5 <Yank5@nycstudents.net>
2023-03-28 12:40:07 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,67 @@
+import com.sun.jdi.ArrayReference;
+
 import java.io.*;
 import java.net.*;
-import java.util.Properties;
+import java.util.*;
 
-public class Main {
+public class Main  {
     public static void main(String[] args) {
 /*        System.out.println("test port number (or test all ports -1)");
         Scanner scanner=new Scanner(System.in);
         int  x = scanner.nextInt();*/
 
 //        Socket socket=new Socket();
-        Properties z=System.getProperties();
+
+
+
+        var xx=java.util.UUID.randomUUID();
+        var gz=java.util.Base64.getDecoder();
+
+
+
+        LinkedList linkedList=new LinkedList<>();
+
+        Vector<Object> vector=new Vector<Object>();
+
+        ArrayDeque arrayDeque=new ArrayDeque<>();
+
+        Stack stack=new Stack();
+
+        List list= new ArrayList<>();
+
+
+        ArrayList arrayList=new ArrayList<>();
+
+
+
+        System.out.println(gz);
+
+
+
+        System.out.println("\n\n\n\n\n\n");
+
+
+
+//        var p=System.getenv();
+//        for(var o:p.keySet()){
+//            System.out.print(o+" = ");
+//            System.out.println(p.get(o));
+//        }
 
 
 
 
 
-
-
-
-
+//Properties z=System.getProperties();
+//
 //        for(var p:z.keySet()){
-//            System.out.print(p+" : ");
+//            System.out.print(p+" = ");
 //            System.out.println(z.get(p));
 //        }
 
 
 
-//        var p=System.getenv();
-//
-//
-//
-//        for(var o:p.keySet()){
-//
-//            System.out.print(o+" : ");
-//            System.out.println(p.get(o));
-//        }
+
 
 
 
