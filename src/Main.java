968f857ea896bebffa40cdb452485b7d7c02dad2
Yank5 <Yank5@nycstudents.net>
2023-03-31 12:53:21 -0400

    a

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,58 @@
-import com.sun.jdi.ArrayReference;
-
+import java.awt.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.util.List;
 
 public class Main  {
     public static void main(String[] args) {
-/*        System.out.println("test port number (or test all ports -1)");
+        System.out.println("test port number (or test all ports -1)");
         Scanner scanner=new Scanner(System.in);
-        int  x = scanner.nextInt();*/
+         int m= scanner.nextInt();
 
 //        Socket socket=new Socket();
 
-
-
-        var xx=java.util.UUID.randomUUID();
-        var gz=java.util.Base64.getDecoder();
-
-
-
-        LinkedList linkedList=new LinkedList<>();
-
-        Vector<Object> vector=new Vector<Object>();
-
-        ArrayDeque arrayDeque=new ArrayDeque<>();
-
-        Stack stack=new Stack();
-
-        List list= new ArrayList<>();
-
-
-        ArrayList arrayList=new ArrayList<>();
+//
+//        System.out.println("\u001b[48;5;244m"+"test");
+        System.out.print("\u001b[?5h");
 
 
 
-        System.out.println(gz);
+        while(true){
+            System.out.print("\u001b[?5h");
+            var r=scanner.next();
+            System.out.print("\u001b[?5l");
+        }
+
+
+//        var xx=java.util.UUID.randomUUID();
+//        var gz=java.util.Base64.getDecoder();
+//
+//
+//
+//
+//
+//
+//        LinkedList linkedList=new LinkedList<>();
+//
+//        Vector<Object> vector=new Vector<Object>();
+//
+//        ArrayDeque arrayDeque=new ArrayDeque<>();
+//
+//        Stack stack=new Stack();
+//
+//        List list= new ArrayList<>();
+//
+//
+//        ArrayList arrayList=new ArrayList<>();
 
 
 
-        System.out.println("\n\n\n\n\n\n");
+//        System.out.println(gz);
+
+
+
+
 
 
 
@@ -68,33 +82,33 @@ public class Main  {
 
 
 
-        try {
-            Inet4Address inet4Address = (Inet4Address) InetAddress.getByName("google.com");
-
-
-//            for(var x:NetworkInterface.networkInterfaces().toArray()){
-//                System.out.println(x);
-//            }
-
-            var x = NetworkInterface.getByIndex(1).getInetAddresses();
-
-
-
-
-
-
-
-
-
-
-
-//            System.out.println(inet4Address.isReachable(900));
-
-
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try {
+//            Inet4Address inet4Address = (Inet4Address) InetAddress.getByName("google.com");
+//
+//
+////            for(var x:NetworkInterface.networkInterfaces().toArray()){
+////                System.out.println(x);
+////            }
+//
+//            var x = NetworkInterface.getByIndex(1).getInetAddresses();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+////            System.out.println(inet4Address.isReachable(900));
+//
+//
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
 
     }
 
